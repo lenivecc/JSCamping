c840f408c44b2dc5bc6a26fd4965a630295bbfc0
lenivecc <lenivecnew@gmail.com>
2020-11-08 20:12:58 +0300

    add extra3

---
+++ Extratasks/task3.1/script.js
@@ -0,0 +1,13 @@
+let searchMaxSum = (arr) => {
+let maxSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let sumFixedStart = 0;
+    for (let j = i; j < arr.length; j++) {
+      sumFixedStart += arr[j];
+      maxSum = Math.max(maxSum, sumFixedStart);
+    }
+  }
+  return maxSum;
+};
+
+console.log(searchMaxSum([-1,5-9,10,-9,4,7,-5]));
