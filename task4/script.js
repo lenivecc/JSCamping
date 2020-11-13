fce986bb778105a9bb95a865ea785b0666f3a70d
lenivecc <lenivecnew@gmail.com>
2020-11-14 00:21:17 +0300

    complete4

--- task4/script.js
+++ task4/script.js
@@ -1,8 +1,8 @@
 class Message{
     constructor(msg){
-        this._id=`${Date.now()}`;
-        this._author="User"
-        this._createdAt=new Date();
+        this._id=msg.id;
+        this._author=msg.author;
+        this._createdAt=msg.createdAt;
         this.text=msg.text;
         if(msg.isPersonal){
             this.isPersonal=msg.isPersonal;
@@ -20,23 +20,26 @@ class Message{
     get createdAt(){
         return this._createdAt;
     }
+    set id(id){
+        this._id=id;
+    }
+    set author(author){
+        this._author=author;
+    }
+    set createdAt(createdAt){
+        this._createdAt=createdAt;
+    }
 }
-// let msg=new Message({
-//     text: "test text for test"
-// });
-// msg.id="1";
-
-// console.log(msg);
 class MessageList{
     constructor(msgs){
-        this._array=msgs;
+        this._array=msgs.map(item=>new Message(item));
         this._user="User";
     }
-    get filterObject(){
-        return this.filterObject;
+    get user(){
+        return this._user;
     }
-    get validateObject(){
-        return this.validateObject;
+    set user(user){
+        this._user=user;
     }
     get array(){
         return this._array;
@@ -50,75 +53,82 @@ class MessageList{
     set user(user){
         this._user=user;
     }
+    static filterObject={
+        author: (item, author)=> !author || item.author.toLowerCase().includes(author.toLowerCase()),
+        text: (item, text)=> !text || item.text.toLowerCase().includes(text.toLowerCase()),
+        dateTo: (item, dateTo)=> !dateTo || item.createdAt<dateTo,
+        dateFrom: (item, dateFrom)=> !dateFrom || item.createdAt>dateFrom
+    }; 
     getPage(skip=0,top=10,filterConfig={}){
-        let filterObject={
-            author: (item, author)=> !author || item.author.toLowerCase().includes(author.toLowerCase()),
-            text: (item, text)=> !text || item.text.toLowerCase().includes(text.toLowerCase()),
-            dateTo: (item, dateTo)=> !dateTo || item.createdAt<dateTo,
-            dateFrom: (item, dateFrom)=> !dateFrom || item.createdAt>dateFrom
-        };   
         let result=this.array.slice();
         Object.keys(filterConfig).forEach((key)=>{
-            result=result.filter((item)=>filterObject[key](item,filterConfig[key]));
+            result=result.filter((item)=>MessageList.filterObject[key](item,filterConfig[key]));
         });
         result=result.filter((item)=>{
             if(item.author===this.user || item.to===this.user || item.isPersonal===false || !item.isPersonal){
                 return true;
             }
+            return false;
         });
         return result.sort((a,b)=>{
             return a.createdAt-b.createdAt;
         })
-        .slice(skip,top);  
+        .slice(skip,skip+top);  
     }
     get(id){
-        return this.array.find((item)=>{if(item.id===id){return true}});
+        return this.array.find((item)=>item.id===id);
     }
     add(msg){
-        if(!MessageList.validate(msg)){
+        msg.id=`${Date.now()}`;
+        msg.author=this.user;
+        msg.createdAt=new Date();
+        if(MessageList.validate(msg)){
             let msg_for_adding=new Message(msg);
             this.array.push(msg_for_adding);
         }
     }
-    // addAll(msgs){
-    //     let result=[];
-    //     for(let i in msgs){
-    //         if(!MessageList.validate(msgs[i])){
-    //             result.push(msgs[i]);
-    //         }
-    //         this.array.push(msgs[i]);
-    //     }
-    //     return result;
-    // }
+    addAll(msgs){
+        msgs=msgs.map(item=>new Message(item));
+        let result=[];
+        for(let i in msgs){
+            if(!MessageList.validate(msgs[i])){
+                result.push(msgs[i]);
+            }
+            this.array.push(msgs[i]);
+        }
+        return result;
+    }
+    static validateObject={        
+        text: (item)=>item.text && item.text.length<=200 && item.text.length>0,
+        isPersonal: (item)=> !item.isPersonal || typeof item.isPersonal==="boolean",
+        to: (item)=> !item.to || typeof item.to==="string"
+    };
     static validate(msg){
-        let validateObject={        
-            text: (item)=>item.text && item.text.length<=200 && item.text.length>0,
-            isPersonal: (item)=> !item.isPersonal || typeof item.isPersonal==="boolean",
-            to: (item)=> !item.to || typeof item.to==="string"
-        };
-        return Object.keys(validateObject).every((key)=>validateObject[key](msg));
+        return Object.keys(MessageList.validateObject).every((key)=>MessageList.validateObject[key](msg));
     }
     edit(id, msg){
-        if(this.get(id).author===this.user){
-            if(msg.text){
-                this.get(id).text=msg.text;          
+        let new_msg=new Message(this.get(id));
+            if(new_msg.author===this.user){
+                if(msg.text){
+                    new_msg.text=msg.text;          
+                }
+                if(msg.isPersonal===true){
+                    new_msg.isPersonal=msg.isPersonal;
+                    new_msg.to=msg.to;
+                }
+                if(msg.isPersonal===false){
+                    delete  new_msg.isPersonal;
+                    delete  new_msg.to;
+                }
             }
-            if(msg.isPersonal===true){
-                this.get(id).isPersonal=msg.isPersonal;
-                this.get(id).to=msg.to;
-            }
-            if(msg.isPersonal===false){
-                delete this.get(id).isPersonal;
-                delete this.get(id).to;
-            }
-            if(MessageList.validate(this.get(id))){
-                return true;
-            }
+        if(MessageList.validate(new_msg)){
+            this.array.splice(this.array.indexOf(this.get(id)),1,new_msg);
+            return true;
         }       
         return false;
     }
     remove(id){
-        if(this.get(id) && this.get(id).author===user){
+        if(this.get(id) && this.get(id).author===this.user){
             this.array.splice(this.array.indexOf(this.get(id)),1);
             return true;
         }
@@ -127,11 +137,13 @@ class MessageList{
     clear(){
         this.array=[];
     }
+
 }
 
 
 
 const array_of_messages=[
+
     {
     id: '8',
     createdAt: new Date('2020-10-12T23:20:00'),
@@ -172,7 +184,7 @@ const array_of_messages=[
     {
     id: '13',
     createdAt: new Date('2020-10-12T23:40:00'),
-    author: 'Иванов Иван',
+    author: 'User',
     text: "",
 
     }
@@ -183,16 +195,10 @@ const messageList=new MessageList(array_of_messages);
 // });
 // console.log("Not valid"+messageList.addAll(array_of_messages));
 
-
-
-
 messageList.add({
     text: "test text for test"
 });
 
-
-
-console.log(messageList); 
 console.log(messageList.getPage()); 
 
 messageList.add({
@@ -200,13 +206,18 @@ messageList.add({
     isPersonal: true,
     to: "Other user"
 });
-
 console.log(messageList.getPage()); 
 
-// console.log(messageList.edit("", {text: "edit", isPersonal: false}));
+console.log(messageList.addAll(array_of_messages));
 
-// console.log(messageList.remove(""));
 
+console.log(messageList.getPage(0,10,{author: "Ив"})); 
+
+console.log(messageList.edit("13", {text: "edit", isPersonal: true,to:"Agent777"}));
+// console.log(messageList.getPage()); 
+
+// console.log(messageList.remove("13"));
+console.log(messageList.getPage()); 
 
 console.log(messageList.clear()); 
 console.log(messageList.getPage()); 
